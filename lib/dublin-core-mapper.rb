bb5602838e5cf042212a6b081804b8dd5fc3c03e
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2009-07-02 10:54:15 -0400

    Updated dublin-core-mapper to be a bit more useful.

---
+++ lib/dublin-core-mapper.rb
@@ -0,0 +1,37 @@
+# 
+class DublinCoreMapper
+  ATTRIBUTES = ['title', 'subject', 'description', 'type', 
+    'source', 'relation', 'coverage', 'creator', 
+    'publisher', 'contributor', 'rights', 'date', 
+    'format', 'identifier', 'language', 'audience', 
+    'provenance', 'rights_holder', 'instructional_method', 
+    'accrual_method', 'accrual_periodicity', 'accrual_policy'
+  ]
+  def self.register(&block)
+    new(&block)
+  end
+
+  def initialize(&block)
+    @elements = []
+    instance_eval(&block)
+  end
+  
+  ATTRIBUTES.each do |attrib|
+    define_method(attrib) do |*args|
+      args.any? ? register(attrib, *args) : self[attrib]
+    end
+  end
+
+  def [](key)
+    @elements.inject([]){|mem, element| element[0].to_s == key.to_s ? mem << element[1] : mem }
+  end
+
+  def each
+    @elements.each {|element| yield(element) }
+  end
+
+  private
+  def register(element, value)
+    @elements.push([element, value]) unless value.nil? || value.to_s == ''
+  end
+end
--- test/dublin-core-mapper_test.rb
+++ test/dublin-core-mapper_test.rb
@@ -1,7 +1,45 @@
 require 'test_helper'
 
 class DublinCoreMapperTest < Test::Unit::TestCase
-  should "probably rename this file and start testing for real" do
-    flunk "hey buddy, you should probably rename this file and start testing for real"
+
+  def setup
+    @dublin_core_mapper = DublinCoreMapper.register do |dcm|
+      subject('Numeral representation of 1000 + 200 + 30 + 4')
+      title('1234')
+      subject('Math')
+      subject('')
+      type('')
+    end
+  end
+
+  should 'have a single title' do
+    assert_equal ['1234'], @dublin_core_mapper.title
+    assert_equal ['1234'], @dublin_core_mapper['title']
+  end
+
+  should 'register multiple subjects' do
+    assert_equal ['Numeral representation of 1000 + 200 + 30 + 4', 'Math'], @dublin_core_mapper['subject']
+    assert_equal ['Numeral representation of 1000 + 200 + 30 + 4', 'Math'], @dublin_core_mapper.subject
+  end
+
+  should 'raise no method error if trying to register an undefined attribute' do
+    assert_raises NoMethodError do
+      DublinCoreMapper.register do |dcm|
+        foo('bar')
+      end
+    end
+  end
+
+
+  should 'have an each method that ' do
+    @yielded = []
+    @dublin_core_mapper.each do |key, values|
+      @yielded << [key, values]
+    end
+
+    assert_equal 3, @yielded.size, "Note, we did not register a blank"
+    assert_equal( ['subject', 'Numeral representation of 1000 + 200 + 30 + 4'],  @yielded[0])
+    assert_equal( ['title', '1234'],  @yielded[1])
+    assert_equal( ['subject', 'Math'],  @yielded[2])
   end
 end
